24baf36bf9c41d01085f23b8950c2e09e4f8373f
shinbunbun <takamuneyuto@gmail.com>
2019-08-23 14:47:06 +0900

    first commit

---
+++ code/front/profile/profile.js
@@ -0,0 +1,9 @@
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get('userId');
+const displayName = urlParams.get('displayName');
+const pictureUrl = urlParams.get('pictureUrl');
+const statusMessage = urlParams.get('statusMessage');
+document.getElementsByClassName("card-title")[0].innerHTML = `<b>${displayName}</b>`;
+document.getElementsByClassName("card-subtitle")[0].textContent = userId;
+document.getElementsByClassName("card-text")[0].textContent = statusMessage;
+document.getElementsByClassName("profieImg")[0].src = pictureUrl;
